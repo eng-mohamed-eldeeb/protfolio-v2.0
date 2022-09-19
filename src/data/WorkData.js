aeaa623d341ea58f2e738862fa453b962bec4cd9
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-09-19 14:18:34 +0200

    change a simple data

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,21 +1,22 @@
 export const Work = [
     {
         id:1,
-        name:"My first portfolio",
-        description:"The project is all about me (the programmer). the portfolio main page as a welcoming page.",
-        tags:["HTML","CSS","JS"],
-        
-        demo:"https://eng-mohamed-eldeeb.github.io/protfolio-1/",
-        github:"https://github.com/eng-mohamed-eldeeb/protfolio-1"
-    },
-    {
-        id:2,
         name:"audiophile",
         description:"A shopping site for audio tech",
         tags:["NEXT JS","CSS"],
         
         demo:"https://audiophile-two.vercel.app/",
         github:"https://github.com/eng-mohamed-eldeeb/audiophile.git"
+        
+    },
+    {
+        id:2,
+        name:"My first portfolio",
+        description:"The project is all about me (the programmer). the portfolio main page as a welcoming page.",
+        tags:["HTML","CSS","JS"],
+        
+        demo:"https://eng-mohamed-eldeeb.github.io/protfolio-1/",
+        github:"https://github.com/eng-mohamed-eldeeb/protfolio-1"
         
     },
     {
